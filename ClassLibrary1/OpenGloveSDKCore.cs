6516d892aa73734b6fe62e80327bacbf69131744
Sebastián Meneses Núñez <sdox_thrash@hotmail.com>
2016-08-18 18:17:50 -0300

    Agregado prototipo con WPF. Se separaron los comportamientos de backend y frontend, se usa una clase Core con patron singleton por ahora para acceder a los mappings. Esta tambien se ocupa de guardar y abrir perfiles.
    WPF con funcionalidad basica, solo se puede seleccionar actuador. Falta abrir perfil y guardar perfil.

---
+++ ClassLibrary1/OpenGloveSDKCore.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenGloveSDKBackend
+{
+    class OpenGloveSDKCore
+    {
+        //Simple singleton pattern for SDKCore
+        private static OpenGloveSDKCore core;
+
+        public static OpenGloveSDKCore getCore()
+        {
+            if (core == null)
+            {
+                core = new OpenGloveSDKCore();
+            }
+            return core;
+        }
+
+        private OpenGloveSDKCore()
+        {
+            Mappings = new Dictionary<string, string>();
+        }
+
+        private int ACTUATORS = 20;
+
+        //Solucion temporal
+        private String[] actuators = { "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
+
+        /// <summary>
+        /// Mappings for region (key) and actuators (value).
+        /// </summary>
+        public Dictionary<String, String> Mappings { get; set; }
+
+        public int getActuatorCount()
+        {
+            return this.ACTUATORS;
+        }
+
+        /// <summary>
+        /// Assings an actuator to a desired region.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void assignActuator(String region, String actuator)
+        {
+            if (!actuator.Equals(""))
+            {
+                try
+                {
+                    this.Mappings.Add(region, actuator);
+                }
+                catch (Exception)
+                {
+                    this.Mappings[region] = actuator;
+                }
+            }
+            else
+            {
+                String liberatedActuator;
+                this.Mappings.TryGetValue(region, out liberatedActuator);
+                if (liberatedActuator != null)
+                {
+                    this.Mappings.Remove(region);
+                }
+
+            }
+        }
+
+
+        /// <summary>
+        /// Saves a Dictionary in a XML file with the provided name. 
+        /// In upcoming releases it should recieve a "Hand" object of some kind.
+        /// </summary>
+        /// <param name="mappings"></param>
+        /// <param name="name"></param>
+        private int saveConfiguration(Dictionary<String, String> mappings, String name)
+        {
+            XElement rootXML = new XElement("hand");
+            foreach (KeyValuePair<string, string> mapping in mappings)
+            {
+                XElement mappingXML = new XElement("mapping", new XElement("region", mapping.Key), new XElement("actuator", mapping.Value));
+                rootXML.Add(mappingXML);
+            }
+            rootXML.Save(name);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Opens an OpenGlove XML Configuration File and creates a mappings dictionary.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        Dictionary<String, String> openConfiguration(String fileName)
+        {
+            Dictionary<String, String> openedConfiguration;
+
+            XDocument xml = XDocument.Load(fileName);
+            openedConfiguration = xml.Root.Elements("mapping")
+                               .ToDictionary(c => (string)c.Element("region"),
+                                             c => (string)c.Element("actuator"));
+
+            return openedConfiguration;
+        }
+
+
+    }
+}
---
+++ OpenGlovePrototype2/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using OpenGloveSDKBackend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenGloveSDKConfigurationPrototype2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private OpenGloveSDKCore sdkCore;
+
+        private List<ComboBox> selectors;
+
+        private List<String> actuators;
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            this.sdkCore = OpenGloveSDKCore.getCore();
+            this.initializeSelectors();
+        }
+
+        /// <summary>
+        /// Instantiates and populates all view's comboboxes with the available amount of actuators.
+        /// </summary>
+        private void initializeSelectors()
+        {
+            selectors = new List<ComboBox>();
+
+            foreach (var selector in selectorsGrid.Children.OfType<ComboBox>())
+            {
+                selectors.Add(selector);
+            }
+
+            int actuatorsCount = sdkCore.getActuatorCount();
+            
+            actuators = new List<string>();
+
+            foreach (ComboBox selector in selectors)
+            {
+                selector.Items.Add("");
+            }
+
+            for (int i = 0; i < actuatorsCount; i++)
+            {
+                actuators.Add(i.ToString());
+                foreach (ComboBox selector in selectors)
+                {
+                    selector.Items.Add(i.ToString());
+                }
+            }
+        }
+        /// <summary>
+        /// Erases an actuator from the selectors (ComboBox) that doesn't own it.
+        /// </summary>
+        /// <param name="actuator"></param>
+        /// <param name="owner"></param>
+        private void removeActuator(String actuator, object owner)
+        {
+            foreach (ComboBox selector in this.selectors)
+            {
+                if (((ComboBox)owner) != selector)
+                {
+                    selector.Items.Remove(actuator);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Repopulates an actuator on all selectors (ComboBox) except for it's owner.
+        /// </summary>
+        /// <param name="liberatedActuator"></param>
+        /// <param name="preowner"></param>
+        private void liberateActuator(String liberatedActuator, Object preowner)
+        {
+            foreach (ComboBox selector in this.selectors)
+            {
+                if ( ! selector.Equals(preowner) ) {
+                    selector.Items.Add(liberatedActuator);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Resets all selectors to it's initial selection (index 0).
+        /// </summary>
+        private void resetSelectors()
+        {
+            foreach (ComboBox selector in this.selectors)
+            {
+                selector.SelectedIndex = 0;
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void selectorsSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String selection = (String)((ComboBox)sender).SelectedItem;
+            //String region = (String)((ComboBox)sender).AccessibleName;
+
+            //Si el selector usado poseia otro actuador con anterioridad, este debe ser liberado y añadido a los otros selectores, en general, a todos.
+
+            // Si se selecciona un actuador, la idea es que no se pueda volver a seleccionar en otro punto de la mano.
+
+            String owner = ((ComboBox)sender).TabIndex.ToString();
+            if (!selection.Equals(""))
+            {
+                removeActuator(selection, sender);
+                try
+                {
+                    this.sdkCore.Mappings.Add(owner, selection);
+                }
+                catch (Exception)
+                {
+                    String liberatedActuator = this.sdkCore.Mappings[owner];
+                    liberateActuator(liberatedActuator, sender);
+                    this.sdkCore.Mappings[owner] = selection;
+                }
+            }
+            else
+            {
+                String liberatedActuator;
+                this.sdkCore.Mappings.TryGetValue(owner, out liberatedActuator);
+                if (liberatedActuator != null)
+                {
+                    liberateActuator(liberatedActuator, sender);
+                    this.sdkCore.Mappings.Remove(owner);
+                }
+
+            }
+
+            //refreshMappingsList(this.sdkCore.Mappings);
+        }
+    }
+}
